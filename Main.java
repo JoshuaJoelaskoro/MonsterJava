5642393328a7cd97d3648e7a959a0bab06d46c57
nandanatyo <nandanatyon@gmail.com>
2024-05-14 07:21:34 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+package MonsterJava;
+//
+//import java.util.ArrayList;
+//
+//class GameLoader{
+//    public Game loadGameFromFile(String filename){
+//
+//    }
+//    public void saveGame(Game game, String filename){
+//
+//    }
+//}
+//
+//class Game{
+//    Player player;
+//    Homebase homebase;
+//    ArrayList<Dungeon> dungeons;
+//
+//    public void startGame(){
+//
+//    }
+//    public void loadGame(){
+//
+//    }
+//    public void saveGame(){
+//
+//    }
+//    public void handleException(PokemonException e){
+//
+//    }
+//}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
